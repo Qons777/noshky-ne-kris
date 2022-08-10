69b806a67ab7adc0c9ef5515f258aa905734e206
Qons777 <111006568+Qons777@users.noreply.github.com>
2022-08-10 21:07:23 +0300

    Add files via upload

---
+++ nozkePizdec.java
@@ -0,0 +1,7 @@
+public class nozkePizdec {
+    public static void main(String[] args) {
+        System.out.println(" nozky ne kris");
+        double time = 21.05;
+        System.out.println(time);
+    }
+}
