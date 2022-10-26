2e2a9e608304680e174033352750e930b32a2fe5
Qons777 <111006568+Qons777@users.noreply.github.com>
2022-10-26 23:14:01 +0300

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,21 @@
+package Task;
+
+import static Task.TaskMethod.calculateSumArray;
+
+/**
+ * Найти среднее арифметическое всех элементов массива
+ *
+ * Например:
+ *
+ * Дано: [1,3,5,8] Результат: 4
+ */
+public class task1 {
+    public static void main(String[] args) {
+        int[] values ={1,3,5,8};
+        calculateSumArray(values);
+        System.out.println(calculateSumArray(values));
+    }
+
+
+    }
+
