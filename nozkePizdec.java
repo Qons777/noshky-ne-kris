ba07d7be2ee6a97c0aa20f07e86bfc499a94ca8a
Qons777 <111006568+Qons777@users.noreply.github.com>
2022-10-26 22:43:26 +0300

    Delete nozkePizdec.java

--- nozkePizdec.java
+++
@@ -1,7 +0,0 @@
-public class nozkePizdec {
-    public static void main(String[] args) {
-        System.out.println(" nozky ne kris");
-        double time = 21.05;
-        System.out.println(time);
-    }
-}
